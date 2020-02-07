e683e8735a0c85eec8c0bbde14ae378787b696d1
Tadas Vaitiekunas <ext.tadas.vaitiekunas@tieto.com>
2020-02-07 10:01:44 +0200

    Example enpoint returning data in json

--- clients/src/main/java/com/template/webserver/Controller.java
+++ clients/src/main/java/com/template/webserver/Controller.java
@@ -1,11 +1,17 @@
 package com.template.webserver;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.corda.client.jackson.JacksonSupport;
 import net.corda.core.messaging.CordaRPCOps;
+import net.corda.core.node.NodeInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Define your API endpoints here.
@@ -23,5 +29,13 @@ public class Controller {
     @GetMapping(value = "/templateendpoint", produces = "text/plain")
     private String templateendpoint() {
         return "Define an endpoint here.";
+    }
+
+    @GetMapping(value = "/networkmap", produces = "text/json")
+    private String networkMap() throws JsonProcessingException {
+        ObjectMapper mapper = JacksonSupport.createNonRpcMapper();
+        //ObjectMapper mapper = JacksonSupport.createDefaultMapper(proxy);
+        List<NodeInfo> nodeList = proxy.networkMapSnapshot();
+        return mapper.writeValueAsString(nodeList);
     }
 }
