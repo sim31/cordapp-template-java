672138834c4863fe0c2b1860072d062b0220c3b2
Tadas <vtadas25@gmail.com>
2020-02-16 14:58:00 +0200

    Fix failing test build

--- workflows/src/test/java/com/template/FlowTests.java
+++ workflows/src/test/java/com/template/FlowTests.java
@@ -1,7 +1,7 @@
 package com.template;
 
 import com.google.common.collect.ImmutableList;
-import com.template.flows.Responder;
+import com.template.flows.IOUFlowResponder;
 import net.corda.testing.node.MockNetwork;
 import net.corda.testing.node.MockNetworkParameters;
 import net.corda.testing.node.StartedMockNode;
@@ -19,8 +19,8 @@ public class FlowTests {
     private final StartedMockNode b = network.createNode();
 
     public FlowTests() {
-        a.registerInitiatedFlow(Responder.class);
-        b.registerInitiatedFlow(Responder.class);
+        a.registerInitiatedFlow(IOUFlowResponder.class);
+        b.registerInitiatedFlow(IOUFlowResponder.class);
     }
 
     @Before
